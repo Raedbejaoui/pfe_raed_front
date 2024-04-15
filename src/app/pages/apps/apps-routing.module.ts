52f532fd59dfdb6d6babeabb4970f2faccaaacb7
Raedbejaoui <raed.bejaoui@esprit.tn>
2024-04-15 21:13:46 +0100

    first commit front

---
+++ src/app/pages/apps/apps-routing.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+// Component
+import { CalendarComponent } from './calendar/calendar.component';
+import { ChatComponent } from './chat/chat.component';
+import { EmailComponent } from './email/email.component';
+import { FileManagerComponent } from './file-manager/file-manager.component';
+import { WidgetsComponent } from './widgets/widgets.component';
+import {MessangerComponent} from "./messanger/messanger.component";
+
+
+
+const routes: Routes = [
+
+  {
+
+        path: "calendar",
+        component: CalendarComponent
+    },
+    {
+        path: "chat",
+        component: ChatComponent
+    },
+
+  {
+        path: "email",
+        component: EmailComponent
+    },
+    {
+        path: "file-manager",
+        component: FileManagerComponent
+    },
+    {
+        path: "widgets",
+        component: WidgetsComponent
+    },
+  {
+    path :"messanger/:id",
+    component: MessangerComponent
+  }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class AppsRoutingModule { }
---
+++ src/app/pages/apps/messanger/messanger.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-messanger',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './messanger.component.html',
+  styleUrl: './messanger.component.scss'
+})
+export class MessangerComponent {
+
+}
