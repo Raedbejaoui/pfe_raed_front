d6fca7366cea6e0caa2c9f214ee29bd0aafc6082
jelassiahmed <ahmed.jelassi@esprit.tn>
2024-06-10 23:32:48 +0100

    elasticsearch working

--- src/app/pages/elasticsearch/elasticsearch.component.ts
+++ src/app/pages/elasticsearch/elasticsearch.component.ts
@@ -1,37 +1,70 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {EntrepriseService} from "../../core/services/entreprise.service";
-import {FormsModule} from "@angular/forms";
+import {ElasticSearchService} from "../../core/services/elastic-search.service";
 
 @Component({
   selector: 'app-elasticsearch',
   templateUrl: './elasticsearch.component.html',
-  styleUrl: './elasticsearch.component.scss'
+  styleUrls: ['./elasticsearch.component.scss']
 })
 export class ElasticsearchComponent implements OnInit{
 
   @ViewChild('searchInput', { static: true }) searchInput!: ElementRef;
   breadcrumbItems: any[] = [];
-  constructor(private entrepriseService: EntrepriseService) { }
-enterprises: any[] = [];
+  documents: any[] = [];
+  allDocuments: any[] = []; // New array to store all documents
+
+  constructor(private elasticSearchService: ElasticSearchService) { }
+
   ngOnInit(): void {
-
+    this.getAllDocuments();
   }
-  getEnterprises(): void {
-    const searchPhrase = this.searchInput.nativeElement.value;
 
-    /*if (!searchPhrase|| searchPhrase.length < 3) {
-      // If the search phrase is empty, clear the enterprises array
-      this.enterprises = [];
-      return;
-    }*/
-
-    this.entrepriseService.searchEnterprises(searchPhrase).subscribe(
+  getAllDocuments(): void {
+    this.elasticSearchService.searchAllDocuments<any>().subscribe(
       (data) => {
-        this.enterprises = data;
+        this.allDocuments = data; // Store all documents
+        this.documents = data; // Display all documents
       },
       (error) => {
         console.error('Error:', error);
       }
     );
+  }
+
+  getDocumentById(id: string): void {
+    this.elasticSearchService.getDocumentById<any>(id).subscribe(
+      (data) => {
+        console.log(data);
+      },
+      (error) => {
+        console.error('Error:', error);
+      }
+    );
+  }
+
+  deleteDocumentById(id: string): void {
+    this.elasticSearchService.deleteDocumentById(id).subscribe(
+      (response) => {
+        console.log(response);
+        // After deletion, refresh the list of documents
+        this.getAllDocuments();
+      },
+      (error) => {
+        console.error('Error:', error);
+      }
+    );
+  }
+
+  searchDocuments(): void {
+    const searchTerm = this.searchInput.nativeElement.value.toLowerCase();
+    if (searchTerm) {
+      this.documents = this.allDocuments.filter(document =>
+          document.name.toLowerCase().includes(searchTerm) ||
+          document.description.toLowerCase().includes(searchTerm)
+        // Add more fields if needed
+      );
+    } else {
+      this.documents = this.allDocuments; // If search term is empty, display all documents
+    }
   }
 }
